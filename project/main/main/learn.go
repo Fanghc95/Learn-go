af2853dd2787bf9a41536414e857077a2055afcb
unknown <469248584@qq.com>
2022-05-15 21:01:18 +0800

    MOD

--- project/main/main/learn.go
+++ project/main/main/learn.go
@@ -1,11 +1,35 @@
 package main
 import "fmt"
-func main() {
-	v := 1
-L1:
-	fmt.Println(v)
-	v++
-	if v < 3 {
-		goto L1
+func test1 (a int) int {
+	a = a + 1
+	return a
+}
+
+func test2 (a *int) int {
+	*a = *a + 1
+	return *a
+}
+
+func suma(arr ...int) int {
+	sum := 0
+	for _, v := range arr {
+		sum += v
 	}
+	return sum
+}
+
+func main() {
+	// b := 1
+	// test1(b)
+	// fmt.Println(b)
+	// test2(&b)
+	// fmt.Println(b)
+// 	v := 1
+// L1:
+// 	fmt.Println(v)
+// 	v++
+// 	if v < 3 {
+// 		goto L1
+// 	}
+	fmt.Println(suma(1, 2, 3, 4))
 }
